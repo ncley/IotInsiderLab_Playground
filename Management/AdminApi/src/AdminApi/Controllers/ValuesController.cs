73c7b8dc5dac65b7efcacee7c976520d5fa25a79
Nathan Bell <v-natbel@microsoft.com>
2016-11-10 15:50:50 -0800

    management project admin api, initial

---
+++ Management/AdminApi/src/AdminApi/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminApi.Controllers
+{
+    
+    /// <summary>
+    /// Administrator API for Campaigns
+    /// </summary>
+    [Produces("application/json")]
+    [EnableCors("WideOpenCORSPolicy")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get all Foos
+        /// </summary>
+        /// <param name="pager">paging settings</param>
+        /// <response code="200">Success</response>
+        [Route("api/[controller]")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<string>), 200)]
+        public async Task<IActionResult> GetAll()//[FromQuery]PagerDto pager)
+        {
+            return new OkObjectResult(new List<string> {"hi","world"});
+        }
+    }
+}
---
+++ Management/AdminApi/src/AdminApi/Startup.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.Swagger.Model;
+
+
+namespace AdminApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets();
+            }
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Admin Management API",
+                    Description = "REST services for Iot Device Management Project",
+                    TermsOfService = "See Microsoft IOT Lab TOS",
+                    Contact = new Contact { Name = "Microsoft IOT Insider Lab", Email = "iotinsider@microsoft.com", Url = "https://microsoft.com/en-us/iotinsider" },
+                    License = new License { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+                });
+
+                //Determine base path for the application.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+
+                //Set the comments path for the swagger json and ui.
+                options.IncludeXmlComments(basePath + "\\AdminApi.xml");
+            });
+
+            // Inject an implementation of ISwaggerProvider with defaulted settings applied
+            services.AddSwaggerGen();
+
+            // Enable Cors
+            services.AddCors(options =>
+            {
+                options.AddPolicy("WideOpenCORSPolicy",
+                    builder =>
+                    {
+                        //TODO: consider tightening this down
+                        builder.AllowAnyOrigin().AllowAnyMethod();
+                    });
+
+            });
+
+            // Add framework services.
+            services.AddMvc().AddJsonOptions(a => a.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+//            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+//            loggerFactory.AddDebug();
+//
+//            app.UseMvc();
+
+
+            //app.UseDeveloperExceptionPage();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+//            app.UseCors(builder =>
+//                builder.AllowAnyOrigin()
+//                .AllowAnyHeader()
+//                .AllowAnyMethod());
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
+            app.UseSwaggerUi();
+
+        }
+    }
+}
