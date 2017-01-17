8ef90aedc09f632f6e8ee5099861d881fa442f4f
Nathan Bell <v-natbel@microsoft.com>
2017-01-17 14:02:28 -0800

    more chart tweaks

--- Management/AdminUI_Simple/src/app/pages/devices/list/lineChart.component.ts
+++ Management/AdminUI_Simple/src/app/pages/devices/list/lineChart.component.ts
@@ -3,19 +3,15 @@ import {BaThemeConfigProvider, colorHelper, layoutPaths} from '../../../theme';
 import {BaThemePreloader} from '../../../theme/services';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
-
 import {DeviceService} from './device.service';
 import {BaAmChartThemeService} from './baAmChartTheme.service';
 
 require('style-loader!ammap3/ammap/ammap.css');
-
 require('amcharts3');
 require('amcharts3/amcharts/plugins/responsive/responsive.js');
 require('amcharts3/amcharts/serial.js');
-
 require('ammap3');
 require('ammap3/ammap/maps/js/worldLow');
-
 
 @Component({
   selector: 'line-chart',
@@ -29,7 +25,8 @@ export class LineChart implements OnInit,AfterViewInit{
   @ViewChild('baAmChart') private _selector:ElementRef;
 
   chartData:Object;
-  series:any;
+  series:Array<any>;
+  bar:Array<any>;
 
   constructor (private _baConfig:BaThemeConfigProvider,private _baAmChartThemeService:BaAmChartThemeService, private _deviceService:DeviceService) {
     this._loadChartsLib();
@@ -37,11 +34,10 @@ export class LineChart implements OnInit,AfterViewInit{
 
   ngOnInit () {
     AmCharts.themes.blur = this._baAmChartThemeService.getTheme();
-//    this.getData();//should this move into after init?
   }
 
   ngAfterViewInit () {
-    this.getData();//should this move into after init?
+    this.getData();
   }
 
 
@@ -74,97 +70,53 @@ export class LineChart implements OnInit,AfterViewInit{
 
   getData = () => {
     var me = this;
-    //this._deviceService.getLatestAggregateMessageStats('minute').subscribe(data => {
-    me._deviceService.getAllDevices().subscribe(data => {
-      //  var series = data.map(function(item) {
-      //     return { date: new Date(2013, 0), value: 15000, value0: 19000};//{date:item.windowendtime,value:item.min,value0:item.max};
-      // });
+    me._deviceService.getLatestAggregateMessageStats('minute').subscribe(data => {
+       me.series = data.map(function(item) {
+          return {date: new Date(item.windowendtime),value:item.min,value0:item.median};
+      });
 
-        me.series = [
-          { date: new Date(2012, 11), value: 0, value0: 0 },
-          { date: new Date(2013, 0), value: 15000, value0: 19000},
-          { date: new Date(2013, 1), value: 30000, value0: 20000},
+        // me.series = [
+        //   //{ date: new Date(2012, 11), value: 0, value0: 0 },
+        //   { date: new Date("2017-01-11T23:39:00Z"), value: 1, value0: 6},
+        //   { date: new Date("2017-01-11T23:40:00Z"), value: 1, value0: 7},
+        //   { date: new Date("2017-01-11T23:41:00Z"), value: 1, value0: 5},
+        //   { date: new Date("2017-01-11T23:42:00Z"), value: 1, value0: 4},
+        //   { date: new Date("2017-01-11T23:43:00Z"), value: 1, value0: 1},
 
+        //   // { date: new Date(2013, 1, 1, 1,4,0,0), value: 21000, value0: 25000},
+        //   // { date: new Date(2013, 1, 1, 1,5,0,0), value: 24000, value0: 29000},
+        //   // { date: new Date(2013, 5), value: 31000, value0: 26000},
+        //   // { date: new Date(2013, 6), value: 40000, value0: 25000},
+        //   // { date: new Date(2013, 7), value: 37000, value0: 20000},
+        //   // { date: new Date(2013, 8), value: 18000, value0: 22000},
+        //   // { date: new Date(2013, 9), value: 5000, value0: 26000},
+        //   // { date: new Date(2013, 10), value: 40000, value0: 30000},
+        //   // { date: new Date(2013, 11), value: 20000, value0: 25000},
+        //   // { date: new Date(2014, 0), value: 5000, value0: 13000},
 
-          { date: new Date(2013, 2), value: 25000, value0: 22000},
-          { date: new Date(2013, 3), value: 21000, value0: 25000},
-          { date: new Date(2013, 4), value: 24000, value0: 29000},
-          { date: new Date(2013, 5), value: 31000, value0: 26000},
-          { date: new Date(2013, 6), value: 40000, value0: 25000},
-          { date: new Date(2013, 7), value: 37000, value0: 20000},
-          { date: new Date(2013, 8), value: 18000, value0: 22000},
-          { date: new Date(2013, 9), value: 5000, value0: 26000},
-          { date: new Date(2013, 10), value: 40000, value0: 30000},
-          { date: new Date(2013, 11), value: 20000, value0: 25000},
-          { date: new Date(2014, 0), value: 5000, value0: 13000},
+        //   // { date: new Date(2014, 1), value: 3000, value0: 13000},
+        //   // { date: new Date(2014, 2), value: 1800, value0: 13000},
+        //   // { date: new Date(2014, 3), value: 10400, value0: 13000},
+        //   // { date: new Date(2014, 4), value: 25500, value0: 13000},
+        //   // { date: new Date(2014, 5), value: 2100, value0: 13000},
+        //   // { date: new Date(2014, 6), value: 6500, value0: 13000},
+        //   // { date: new Date(2014, 7), value: 1100, value0: 13000},
+        //   // { date: new Date(2014, 8), value: 17200, value0: 13000},
+        //   // { date: new Date(2014, 9), value: 26900, value0: 13000},
+        //   // { date: new Date(2014, 10), value: 14100, value0: 13000},
+        //   // { date: new Date(2014, 11), value: 35300, value0: 13000},
+        //   // { date: new Date(2015, 0), value: 54800, value0: 13000},
+        //   // { date: new Date(2015, 1), value: 49800, value0: 13000}
+        // ];
 
-          { date: new Date(2014, 1), value: 3000, value0: 13000},
-          { date: new Date(2014, 2), value: 1800, value0: 13000},
-          { date: new Date(2014, 3), value: 10400, value0: 13000},
-          { date: new Date(2014, 4), value: 25500, value0: 13000},
-          { date: new Date(2014, 5), value: 2100, value0: 13000},
-          { date: new Date(2014, 6), value: 6500, value0: 13000},
-          { date: new Date(2014, 7), value: 1100, value0: 13000},
-          { date: new Date(2014, 8), value: 17200, value0: 13000},
-          { date: new Date(2014, 9), value: 26900, value0: 13000},
-          { date: new Date(2014, 10), value: 14100, value0: 13000},
-          { date: new Date(2014, 11), value: 35300, value0: 13000},
-          { date: new Date(2015, 0), value: 54800, value0: 13000},
-          { date: new Date(2015, 1), value: 49800, value0: 13000}
-        ];
-
-      //this.chartData = this.buildChartFromSeries(series, layoutColors, graphColor);
-      var foo = 1;
     },
     error => {
-      var bar = 2;
+      //var bar = 2;
     },
     () => {
-      me.setChartObject(me.buildChartFromSeries(me.series));
-      //me.chartData = me.buildChartFromSeries(me.series)
+      me.setChartObject(me.buildChartConfigFromSeries(me.series));
     });
 
-
-    //this._deviceService.getAllDevices().subscribe(data => this.buildChartFromSeries());
-
-    // var data = this._deviceService.getLatestAggregateMessageStats('minute');
-    // var series = data.map(function(item) {
-    //       return { date: new Date(2013, 0), value: 15000, value0: 19000};//{date:item.windowendtime,value:item.min,value0:item.max};
-    //   });
-
-      // var series = [
-      //   { date: new Date(2012, 11), value: 0, value0: 0 },
-      //   { date: new Date(2013, 0), value: 15000, value0: 19000},
-      //   { date: new Date(2013, 1), value: 30000, value0: 20000},
-
-
-      //   { date: new Date(2013, 2), value: 25000, value0: 22000},
-      //   { date: new Date(2013, 3), value: 21000, value0: 25000},
-      //   { date: new Date(2013, 4), value: 24000, value0: 29000},
-      //   { date: new Date(2013, 5), value: 31000, value0: 26000},
-      //   { date: new Date(2013, 6), value: 40000, value0: 25000},
-      //   { date: new Date(2013, 7), value: 37000, value0: 20000},
-      //   { date: new Date(2013, 8), value: 18000, value0: 22000},
-      //   { date: new Date(2013, 9), value: 5000, value0: 26000},
-      //   { date: new Date(2013, 10), value: 40000, value0: 30000},
-      //   { date: new Date(2013, 11), value: 20000, value0: 25000},
-      //   { date: new Date(2014, 0), value: 5000, value0: 13000},
-
-      //   { date: new Date(2014, 1), value: 3000, value0: 13000},
-      //   { date: new Date(2014, 2), value: 1800, value0: 13000},
-      //   { date: new Date(2014, 3), value: 10400, value0: 13000},
-      //   { date: new Date(2014, 4), value: 25500, value0: 13000},
-      //   { date: new Date(2014, 5), value: 2100, value0: 13000},
-      //   { date: new Date(2014, 6), value: 6500, value0: 13000},
-      //   { date: new Date(2014, 7), value: 1100, value0: 13000},
-      //   { date: new Date(2014, 8), value: 17200, value0: 13000},
-      //   { date: new Date(2014, 9), value: 26900, value0: 13000},
-      //   { date: new Date(2014, 10), value: 14100, value0: 13000},
-      //   { date: new Date(2014, 11), value: 35300, value0: 13000},
-      //   { date: new Date(2015, 0), value: 54800, value0: 13000},
-      //   { date: new Date(2015, 1), value: 49800, value0: 13000}
-      // ];
-    
   }
 
 
@@ -174,7 +126,7 @@ export class LineChart implements OnInit,AfterViewInit{
     this.initChart(chart);
   }
 
-  buildChartFromSeries = (series) => {
+  buildChartConfigFromSeries = (series) => {
     var layoutColors = this._baConfig.get().colors;
     var graphColor = this._baConfig.get().colors.custom.dashboardLineChart;
 
@@ -190,6 +142,7 @@ export class LineChart implements OnInit,AfterViewInit{
       ,
       categoryField: 'date',
       categoryAxis: {
+        "minPeriod": "mm",
         parseDates: true,
         gridAlpha: 0,
         color: layoutColors.defaultText,
@@ -230,7 +183,7 @@ export class LineChart implements OnInit,AfterViewInit{
         }
       ],
       chartCursor: {
-        categoryBalloonDateFormat: 'MM YYYY',
+        categoryBalloonDateFormat: 'JJ:NN, DD MMMM',
         categoryBalloonColor: '#4285F4',
         categoryBalloonAlpha: 0.7,
         cursorAlpha: 0,
@@ -238,7 +191,7 @@ export class LineChart implements OnInit,AfterViewInit{
         valueLineBalloonEnabled: true,
         valueLineAlpha: 0.5
       },
-      dataDateFormat: 'MM YYYY',
+      dataDateFormat: 'YYYY-MM-DD HH:NN:SS',
       export: {
         enabled: true
       },
